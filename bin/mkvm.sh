add25d80ed3cdc1709626e9938716f7b0844794e
Philippe Lawson <philippe.lawson@lawson-it.com>
2020-11-02 23:38:02 +0000

    Added dynamic configuration

--- bin/mkvm.sh
+++ bin/mkvm.sh
@@ -1,55 +1,13 @@
 #/bin/bash
 
 function usage {
-	echo "$0: [-h] | --name VM_NAME --user USERNAME [--mac MAC_ADDRESS] [--cpu #_CPU] [--vmhosts CUSTOM_HOSTS_FILE] [--memory MEMORY_IN_MB]"
+	echo "$0: [-h] | --name VM_NAME --user USERNAME --type server_or_desktop --basefolder BASE_FOLDER [--mac MAC_ADDRESS] [--cpu #_CPU] [--memory MEMORY_IN_MB] [--disk STORAGE_IN_GB]"
 	exit
-}
-
-function generate_iso {
-	# Parameters: $1 VM name, $2 vm owner
-	if [[ $# -ne 3 ]]; then
-		echo ""
-		echo "ERROR: generate_iso wrong number of parameters ($#): $@"
-		echo ""
-		exit
-	fi
-	VM_NAME=$1
-	VM_OWNER=$2
-	VM_HOSTS=$3
-	THE_DIR=$(dirname $0)
-
-	if [[ ${USER} != "root" ]]; then
-		echo ""
-		echo "ERROR: Script must be run as root"
-		echo ""
-	fi
-
-	# Set host name
-	cp -f $THE_DIR/../unattended_iso_customization/ks.preseed /opt/ubuntuiso/ks.preseed
-	sed -r -i 's@^d-i netcfg/get_hostname string.*$@d-i netcfg/get_hostname string\t\t'${VM_NAME}'@' /opt/ubuntuiso/ks.preseed
-	# Customize post install script
-	cp -f $THE_DIR/../unattended_iso_customization/post-install.sh /opt/ubuntuiso/post-install.sh
-	chmod 755 /opt/ubuntuiso/post-install.sh
-	sed -r -i 's@^new_hostname.*$@new_hostname="'${VM_NAME}'"@' /opt/ubuntuiso/post-install.sh
-	# Docker customization
-	cp -f $THE_DIR/../unattended_iso_customization/custdocker /opt/ubuntuiso/custdocker
-	cp -f $THE_DIR/../unattended_iso_customization/daemon.json /opt/ubuntuiso/daemon.json
-	sed -r -i 's@your_local_insecure_registry@192\.168\.1\.101:5000@' /opt/ubuntuiso/daemon.json
-	# Copy hosts file customization
-	cp -f $THE_DIR/../unattended_iso_customization/$VM_HOSTS /opt/ubuntuiso/vm_hosts
-	# Copy sudoers
-	cp -f $THE_DIR/../unattended_iso_customization/ubuntu /opt/ubuntuiso/ubuntu_sudoer
-
-	# Generate iso
-	rm -f /home/$VM_OWNER/virtual_machines/my_iso/ubuntu-16.04.3-server-amd64-silent_$VM_NAME.iso
-	mkisofs -D -r -V "ATTENDLESS_UBUNTU" -cache-inodes -J -l -b isolinux/isolinux.bin \
-		-c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table \
-		-o /home/$VM_OWNER/virtual_machines/my_iso/ubuntu-16.04.3-server-amd64-silent_$VM_NAME.iso /opt/ubuntuiso
 }
 
 function create_vm {
 	# Parameters: $1 VM name, $2 vm owner
-        if [[ $# -lt 3 ]]; then
+        if [[ $# -lt 7 ]]; then
                 echo ""
                 echo "ERROR: generate_iso wrong number of parameters ($#): $@"
                 echo ""
@@ -57,16 +15,13 @@ function create_vm {
         fi
         VM_NAME=$1
         VM_OWNER=$2
-	VM_MEMORY=${3}
-	VM_CPU=${4}
-	MAC_ADDRESS=$5
-
-	if [[ ! -f /home/$VM_OWNER/virtual_machines/my_iso/ubuntu-16.04.3-server-amd64-silent_$VM_NAME.iso ]]; then
-		echo ""
-		echo "ISO /home/$VM_OWNER/virtual_machines/my_iso/ubuntu-16.04.3-server-amd64-silent_$VM_NAME.iso does't exist, please run script as root for iso to be generated"
-		echo ""
-		exit
-	fi
+	VM_TYPE=$3
+	VM_BASE_FOLDER=$4
+	VM_MEMORY=$5
+	VM_CPU=$6
+	VM_DISK=$7
+	MAC_ADDRESS=$8
+	THE_DIR=$(dirname $0)
 
 	if [[ ${USER} != "$VM_OWNER" ]]; then
                 echo ""
@@ -74,25 +29,45 @@ function create_vm {
                 echo ""
         fi
 
-	vboxmanage createvm --name $VM_NAME --basefolder /home/$VM_OWNER/virtual_machines --register
-	vboxmanage modifyvm $VM_NAME --ostype Ubuntu_64
-	vboxmanage modifyvm $VM_NAME --memory ${VM_MEMORY}
-	vboxmanage modifyvm $VM_NAME --acpi on
-	vboxmanage modifyvm $VM_NAME --ioapic on
-	vboxmanage modifyvm $VM_NAME --cpus ${VM_CPU}
-	vboxmanage modifyvm $VM_NAME --nic1 bridged --bridgeadapter1 enp5s0    
+	# autoinstall config
+	BASE_WEB_FOLDER="$VM_BASE_FOLDER"/www
+	WEB_FOLDER="$BASE_WEB_FOLDER"/ubuntu
+	mkdir -p "$WEB_FOLDER"
+	cp -f $THE_DIR/../unattended_iso_customization/user-data."$VM_TYPE" "$WEB_FOLDER"/user-data
+	sed -r -i 's@the_host_name@'${VM_NAME}'@' "$WEB_FOLDER"/user-data
+	touch "$WEB_FOLDER"/meta-data
+
+	BASE_FOLDER="$VM_BASE_FOLDER"/virtualbox
+	ISO_MEDIA="$BASE_FOLDER"/my_iso/ubuntu-20.04.1-live-server-amd64.iso
+	VDI_NAME=/synology1/VM/"$VM_NAME".vdi
+
+	vboxmanage createvm --name $VM_NAME --basefolder "$BASE_FOLDER" --ostype Ubuntu_64 --register
+	vboxmanage modifyvm $VM_NAME --memory ${VM_MEMORY} --nic1 bridged --bridgeadapter1 enp5s0
+	if [[ "VM_TYPE" = "desktop" ]]; then
+		vboxmanage modifyvm $VM_NAME --vrde on --vrdeport 3389
+	fi
 	if [[ ! -z "${MAC_ADDRESS}" ]]; then
 		vboxmanage modifyvm $VM_NAME --macaddress1 $MAC_ADDRESS
 	fi
-	vboxmanage createhd --filename /synology/vm/virtualbox/$VM_NAME -size 1097152 --variant Standard --format VDI
-	vboxmanage storagectl $VM_NAME --name "SATA Controller" --add sata --bootable on
-	vboxmanage storageattach $VM_NAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium /synology/vm/virtualbox/$VM_NAME.vdi
-	vboxmanage storagectl $VM_NAME --name "IDE Controller" --add ide
-	vboxmanage storageattach $VM_NAME --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium /home/$VM_OWNER/virtual_machines/my_iso/ubuntu-16.04.3-server-amd64-silent_$VM_NAME.iso
+	# vboxmanage modifyvm $VM_NAME --acpi on
+	# vboxmanage modifyvm $VM_NAME --ioapic on
+	vboxmanage modifyvm $VM_NAME --cpus ${VM_CPU}
+	vboxmanage createhd --filename "$VDI_NAME" --size "$VM_DISK" --format VDI
+	vboxmanage storagectl $VM_NAME --name "SATA" --add sata --bootable on
+	vboxmanage storageattach $VM_NAME --storagectl "SATA" --port 0 --type hdd --medium "$VDI_NAME"
+	vboxmanage storageattach $VM_NAME --storagectl "SATA" --port 15 --type dvddrive --medium "$ISO_MEDIA"
+	
+	cd "$BASE_WEB_FOLDER"
+	nohup python3 -m http.server 3003 2>&1 1>/dev/null &
+	HTTP_SRV=$!
+	echo $HTTP_SRV
+
 	vboxmanage startvm $VM_NAME --type headless
 	# Comment the previousline and uncomment the below command when debugging your unattended iso install.
 	# Note that uncommenting the below line will display the X11 install UI. In that case set your $DISPLAY variable accordingly before running the script or the istall might hang.
 	#vboxmanage startvm $VM_NAME
+	sleep 1800
+	kill -15 $HTTP_SRV
 }
 
 POSITIONAL=()
@@ -100,40 +75,50 @@ while [[ $# -gt 0 ]]
 do
 	key="$1"
 	case $key in
-		--name)
+	--name)
 		VM_NAME="$2"
 		shift # past argument
 		shift # past value
 		;;
-		--user)
+	--user)
 		VM_OWNER="$2"
 		shift # past argument
 		shift # past value
 		;;
-		--mac)
+	--mac)
 		MAC_ADDRESS="$2"
 		shift # past argument
 		shift # past value
 		;;
-		--cpu)
+	--cpu)
 		VM_CPU="$2"
 		shift # past argument
 		shift # past value
 		;;
-		--vmhosts)
-		VM_HOSTS="$2"
-		shift # past argument
-		shift # past value
-		;;
-		--memory)
+	--memory)
 		VM_MEMORY="$2"
 		shift # past argument
 		shift # past value
 		;;
-		-h)
+	--type)
+		VM_TYPE="$2"
+		shift # past argument
+		shift # past value
+		;;
+	--basefolder)
+		VM_BASE_FOLDER="$2"
+		shift # past argument
+		shift # past value
+		;;
+	--disk)
+		VM_DISK="$2"
+		shift # past argument
+		shift # past value
+		;;
+	-h)
 		usage
 		;;
-		*) # unknown option
+	*) # unknown option
 		POSITIONAL+=("$1") # save it in an array for later
 		shift # past argument
 		;;
@@ -149,6 +134,19 @@ if [[ -z "${VM_OWNER// }" ]]; then
 	usage
 fi
 
+if [[ -z "${VM_TYPE// }" ]]; then
+	usage
+fi
+
+if [[ "$VM_TYPE" != "server" && "$VM_TYPE" != "desktop" ]]; then
+	echo "Invalid --type value: $VM_TYPE"
+	usage
+fi
+
+if [[ -z "${VM_BASE_FOLDER// }" ]]; then
+	usage
+fi
+
 if [[ -z "${MAC_ADDRESS// }" ]]; then
 	MAC_ADDRESS=""
 fi
@@ -157,18 +155,13 @@ if [[ -z "${VM_CPU// }" ]]; then
 	VM_CPU="3"
 fi
 
-if [[ -z "${VM_HOSTS// }" ]]; then
-	VM_HOSTS="vm_hosts"
-fi
-
 if [[ -z "${VM_MEMORY// }" ]]; then
 	VM_MEMORY="15360"
 fi
 
-
-if [[ ${USER} == "root" ]]; then
-	generate_iso $VM_NAME $VM_OWNER $VM_HOSTS
-	sudo -u $VM_OWNER /bin/bash $0 --name $VM_NAME --user $VM_OWNER --mac $MAC_ADDRESS --cpu $VM_CPU
-else
-	create_vm $VM_NAME $VM_OWNER $VM_MEMORY $VM_CPU $MAC_ADDRESS
+if [[ -z "${VM_DISK// }" ]]; then
+	VM_DISK="20480"
 fi
+
+
+create_vm $VM_NAME $VM_OWNER $VM_TYPE $VM_BASE_FOLDER $VM_MEMORY $VM_CPU $VM_DISK $MAC_ADDRESS
